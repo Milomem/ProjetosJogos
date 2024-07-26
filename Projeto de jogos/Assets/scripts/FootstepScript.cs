2b450a88204f7362048bfdb7f4a7649f431cec28
Keven Alison <kevenalison13@gmail.com>
2024-07-26 01:11:45 -0300

    Footstep sound ajust

--- Projeto de jogos/Assets/scripts/FootstepScript.cs
+++ Projeto de jogos/Assets/scripts/FootstepScript.cs
@@ -6,14 +6,6 @@ public class FootstepScript : MonoBehaviour
 {
     public AudioSource footstepAudioSource;
     public AudioClip footstepClip;
-    public float stepInterval = 0.5f; // Intervalo entre passos
-    private float stepTimer;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        stepTimer = stepInterval;
-    }
 
     // Update is called once per frame
     void Update()
@@ -22,33 +14,19 @@ public class FootstepScript : MonoBehaviour
 
         if (isWalking)
         {
-            stepTimer -= Time.deltaTime;
-            if (stepTimer <= 0)
+            if (!footstepAudioSource.isPlaying)
             {
-                PlayFootstep();
-                stepTimer = stepInterval;
+                footstepAudioSource.clip = footstepClip;
+                footstepAudioSource.loop = true; // Ativa o loop para o áudio
+                footstepAudioSource.Play();
             }
         }
         else
         {
-            StopFootsteps();
-            stepTimer = stepInterval; // Reseta o timer quando para de andar
-        }
-    }
-
-    void PlayFootstep()
-    {
-        if (!footstepAudioSource.isPlaying)
-        {
-            footstepAudioSource.PlayOneShot(footstepClip);
-        }
-    }
-
-    void StopFootsteps()
-    {
-        if (footstepAudioSource.isPlaying)
-        {
-            footstepAudioSource.Stop();
+            if (footstepAudioSource.isPlaying)
+            {
+                footstepAudioSource.Stop();
+            }
         }
     }
 }
