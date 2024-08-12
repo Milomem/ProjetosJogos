d7ec07981ac9d79ec80918c1c1f12cf8ca7c4e64
Keven Alison <kevenalison13@gmail.com>
2024-08-12 01:53:45 -0300

    Adicao menu e transição entre fases

---
+++ Projeto de jogos/Assets/scripts/PlayButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Image buttonImage;
+    public Sprite normalSprite;
+    public Sprite hoverSprite;
+    public Vector3 normalScale = new Vector3(1f, 1f, 1f);
+    public Vector3 hoverScale = new Vector3(1.1f, 1.1f, 1.1f);
+
+    // Quando o mouse entra no botão
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonImage.sprite = hoverSprite;
+        transform.localScale = hoverScale;
+    }
+
+    // Quando o mouse sai do botão
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonImage.sprite = normalSprite;
+        transform.localScale = normalScale;
+    }
+}
